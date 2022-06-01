d3f7d38a627aadda56adc9f58f7c4218c7fa2129
knowledgefoundation <ambarish.ganguly@gmail.com>
2022-06-01 09:46:56 +0530

    Changes to Azure Container Apps

--- redwine-client/redwine.py
+++ redwine-client/redwine.py
@@ -3,8 +3,8 @@ import json
 
 # url = 'http://localhost:5000/predict'
 # url = 'http://redwineapp.centralindia.azurecontainer.io:5000/predict'
-url ='http://ae83bef5e10594984b04e065f3060575-1730314012.us-east-1.elb.amazonaws.com/predict'
-
+# url ='http://ae83bef5e10594984b04e065f3060575-1730314012.us-east-1.elb.amazonaws.com/predict'
+url = " https://redwineapp.mangomushroom-6e2198d7.canadacentral.azurecontainerapps.io/predict"
 data = [7.3,0.65,0,1.2,0.065,15,21,0.9946,3.39,0.47,11]
 j_data = json.dumps(data)
 print(j_data)
